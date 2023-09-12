cac75d5b564d5ae587ba217485c02e7cdb52864b
Jess-Jiaqi <li.jiaqi5@northeastern.edu>
2023-09-11 23:38:10 -0700

    Initial Commit

---
+++ INFO6350_Smartphone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  INFO6350_Smartphone
+//
+//  Created by Jiaqi Li on 9/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
