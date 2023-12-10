e5ab5dd4899d7abd6382277c40599a06be97197f
Jess-Jiaqi <98194175+Jess-Jiaqi@users.noreply.github.com>
2023-12-09 16:49:54 -0800

    Add NameAndPhoneProtocolAndDelegate

---
+++ final/NameAndPhoneProtocol/NameAndPhoneProtocol/NamePhoneNumberViewController.swift
@@ -0,0 +1,31 @@
+//
+//  NamePhoneNumberViewController.swift
+//  NameAndPhoneProtocol
+//
+//  Created by Jiaqi Li on 12/9/23.
+//
+
+import UIKit
+
+class NamePhoneNumberViewController: UIViewController {
+
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    var sendNamePhoneDelegate: SendNamePhoneNumberDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveNamePhone(_ sender: Any) {
+        guard let name = name.text else {return}
+        guard let phoneNumber = phoneNumber.text else {return}
+        
+        sendNamePhoneDelegate?.sendNamePhoneNumber(name: name, phoneNumber: phoneNumber)
+        
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+}
---
+++ final/NameAndPhoneProtocol/NameAndPhoneProtocol/SendNamePhoneNumberDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SendNamePhoneNumberDelegate.swift
+//  NameAndPhoneProtocol
+//
+//  Created by Jiaqi Li on 12/9/23.
+//
+
+import Foundation
+
+protocol SendNamePhoneNumberDelegate{
+    func sendNamePhoneNumber(name: String, phoneNumber: String)
+}
